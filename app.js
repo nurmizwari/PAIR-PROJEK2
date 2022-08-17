cf5118a4adb5bc3718abdc9efcc19fdf8e861b38
aiqzky <aikizkypaujan3@gmail.com>
2022-08-17 16:13:39 +0700

    landing

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = 3000
 // const Controller = require('./controller')
 
 app.set('view engine','ejs')
+app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({extended:true}))
 app.use(session({
   secret: 'rahasia', // harus ada // untuk ngamankan session kita
